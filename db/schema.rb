a688451f0c54b7472d0f50f6149892bbbae49efb
Christoph Matthies <chrisma@users.noreply.github.com>
2022-10-24 19:30:15 +0200

    Author

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_21_112809) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_183732) do
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "homepage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
