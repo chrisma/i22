0b52cf0bb51cfa9ebc0f6312686c1be059bae820
Christoph Matthies <chrisma@users.noreply.github.com>
2022-10-27 13:02:20 +0200

    Pass #4

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,18 @@
 class AuthorsController < ApplicationController
     def new
     end
+
+    # https://guides.rubyonrails.org/v6.0/getting_started.html#saving-data-in-the-controller
+    def create
+        @author = Author.new(author_params)
+    
+        @author.save
+        # Do not issue a redirect_to @author, as this will error (there is no code for showing authors yet). Instead you can redirect_to root_path, notice: 'Success!'.
+        redirect_to root_path, notice: 'Success!'
+    end
+
+    private
+        def author_params
+            params.require(:author).permit(:first_name, :last_name, :homepage)
+        end
 end
