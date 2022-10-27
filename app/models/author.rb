a7ff2e3e9e62b4ca4f92c3c285807f631c504303
Christoph Matthies <chrisma@users.noreply.github.com>
2022-10-27 12:49:00 +0200

    Pass #3

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,19 @@
 class Author < ApplicationRecord
+
+    def name()
+        # Call the methods `first_name` and `last_name` and concatenate their outputs 
+        
+        # Version 1 (explicit function calls, explicit return)
+        # return first_name() + ' ' + last_name()
+
+        # Version 2 (string interpolation, explicit function calls, explicit return)
+        # return "#{first_name()} #{last_name()}"
+
+        # Version 3 (implicit calls, implicit return)
+        # first_name + ' ' + last_name
+
+        # Version 4 (string interpolation, implicit calls, implicit return)
+        "#{first_name} #{last_name}"
+    end
+
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,21 +1,31 @@
 require 'rails_helper'
 
 describe "The author model", type: :model do
+  before :each do
+    @first = 'Alan'
+    @last = 'Turing'
+    @homepage = 'http://wikipedia.org/Alan_Turing'
+    @author = Author.new(first_name: @first, last_name: @last, homepage: @homepage)
+  end
+
+  it "should have a name and homepage" do
+    expect(@author.first_name).to eq(@first)
+    expect(@author.last_name).to eq(@last)
+    expect(@author.homepage).to eq(@homepage)
+  end
+
   it "should have a name and homepage" do
     first = 'Alan'
     last = 'Turing'
     homepage = 'http://wikipedia.org/Alan_Turing'
-    author = Author.new(first_name: first, last_name: last, homepage: homepage)
+    # author = Author.new({first_name: first, last_name: last, homepage: homepage})
+    author = Author.new({:first_name => first, :last_name => last, :homepage => homepage})
     expect(author.first_name).to eq(first)
     expect(author.last_name).to eq(last)
     expect(author.homepage).to eq(homepage)
   end
 
   it "should have a method called 'name' that returns the full name" do
-    first = 'Alan'
-    last = 'Turing'
-    homepage = 'http://wikipedia.org/Alan_Turing'
-    author = Author.new(first_name: first, last_name: last, homepage: homepage)
-    expect(author.name).to eq(first + ' ' + last)
+    expect(@author.name).to eq(@first + ' ' + @last)
   end
 end
