708c771f8eef05c4403adfdeba155a60492bda5c
Christoph Matthies <chrisma@users.noreply.github.com>
2022-10-27 15:35:04 +0200

    Second test for #9

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -12,4 +12,12 @@ describe "Edit author page", type: :feature do
 	it "should exist at 'edit_author_path' and render withour error" do
 		visit edit_author_path(@alan)
 	end
+
+	it "should persist changes made in the form" do
+		visit edit_author_path(@alan)
+		new_first = 'Bob'
+		page.fill_in 'author[first_name]', with: new_first
+		find('input[type="submit"]').click
+		expect(@alan.reload.first_name).to eq(new_first)
+	end	
 end
