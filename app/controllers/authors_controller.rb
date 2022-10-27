a8c0a7b77c02830bc2f488b2391f64eb9a43ac73
Christoph Matthies <chrisma@users.noreply.github.com>
2022-10-27 15:18:55 +0200

    Pass #8

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,15 +9,18 @@ class AuthorsController < ApplicationController
     end
 
     def new
+        @author = Author.new
     end
 
     # https://guides.rubyonrails.org/v6.0/getting_started.html#saving-data-in-the-controller
     def create
         @author = Author.new(author_params)
     
-        @author.save
-        # Do not issue a redirect_to @author, as this will error (there is no code for showing authors yet). Instead you can redirect_to root_path, notice: 'Success!'.
-        redirect_to root_path, notice: 'Success!'
+        if @author.save
+            redirect_to @author, notice: 'Success!'
+        else
+          render 'new'
+        end
     end
 
     private
