cae7ed902497656aa0352d015237384ad9fb96b3
Christoph Matthies <chrisma@users.noreply.github.com>
2022-10-27 15:18:37 +0200

    Test for #8

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -25,4 +25,15 @@ describe "New author page", type: :feature do
     edsger = Author.find_by!(last_name: 'Dijkstra')
     expect(edsger.first_name).to eq(first)
   end
+
+  it "should show validation errors" do
+    # Your test can try to create an invalid object (on the new author page) and check whether 'error' is displayed.
+    visit new_author_path
+    first = 'Edsger'
+    page.fill_in 'author[first_name]', with: 'Edsger'
+    # page.fill_in 'author[last_name]', with: 'Dijkstra' # leave empty
+    page.fill_in 'author[homepage]', with: 'https://en.wikipedia.org/wiki/Edsger_W._Dijkstra'
+    find('input[type="submit"]').click
+    expect(page).to have_text('error')
+  end
 end
