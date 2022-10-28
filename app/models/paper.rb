4f865f2b7174590cdd691b7b78827c391b93995c
Christoph Matthies <chrisma@users.noreply.github.com>
2022-10-28 12:05:03 +0200

    Pass #16

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,3 +1,5 @@
 class Paper < ApplicationRecord
-    validates :title, :venue, :year, presence: true
+    validates :title, :venue, presence: true
+    # https://guides.rubyonrails.org/v6.0/active_record_validations.html#numericality
+    validates :year, numericality: { only_integer: true }
 end
