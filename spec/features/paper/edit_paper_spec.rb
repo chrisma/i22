aba31719bb7428434f727b6c8728900fc1b69bae
Christoph Matthies <chrisma@users.noreply.github.com>
2022-10-28 13:15:53 +0200

    Test for #22

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -19,4 +19,23 @@ describe "Edit paper page", type: :feature do
   it "should allow to select paper authors from a multiple select box" do
     expect(page).to have_css('select[multiple]', count: 1)
   end
+
+  # Given a paper with one author
+  # And another author called 'Peter Plagiarist'
+  # When a user visits the paper's edit page
+  # And then selects 'Peter Plagiarist' as author
+  # And submits the form
+  # Then 'Peter Plagiarist' and 'Alan Turing' should be authors of the paper
+  it "should save changes to the author list" do
+    expect(@paper.authors.count).to eq(1)
+    peter = FactoryBot.create(:author, first_name: 'Peter', last_name: 'Plagiarist')
+    visit edit_paper_path(@paper)
+    # Identify select HTML element by its id
+    select(peter.name, from: 'paper_author_ids')
+    submit_button = find('input[type="submit"]')
+    expect{submit_button.click}.to change{@paper.authors.count}.by(1)
+    # Reload the changed paper from the database
+    expect(@paper.reload.authors).to include(peter)
+  end
+
 end
