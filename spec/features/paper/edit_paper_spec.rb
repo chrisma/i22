a95290dd896cac738f22d600cc87e5a747be22d6
Christoph Matthies <chrisma@users.noreply.github.com>
2022-10-28 12:57:21 +0200

    Test for #21

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -11,5 +11,12 @@ describe "Edit paper page", type: :feature do
     page.fill_in 'paper[year]', with: new_year
     find('input[type="submit"]').click
     expect(@paper.reload.year).to eq(new_year)
-  end	
+  end
+
+  # Given a paper
+  # When a user visits the paper's edit page
+  # Then it should have a single multiple select box
+  it "should allow to select paper authors from a multiple select box" do
+    expect(page).to have_css('select[multiple]', count: 1)
+  end
 end
